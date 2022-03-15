0d8529c132563ed5f1cfb28f55278dd2ac7c67d8
Eddiebee <edbeeotudor@gmail.com>
2022-03-15 03:35:55 +0100

    Add Layout and ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import './App.css';
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+
+import theme from './components/theme';
+import { Avatar, Typography } from '@mui/material';
 
 function App() {
   return (
-    <h1>App</h1>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box sx={{ height: 645, bgcolor: '#eeeeee' }}>
+          <Box sx={{ height: 120, bgcolor: '#ff6d00' }} />
+          <Avatar sx={{ width: 120, height: 120, mx: 'auto', mt: -8 }}></Avatar>
+          <Typography textAlign='center' textTransform='uppercase'>Your order from
+          <Typography variant='h6'>Pizza Hut</Typography></Typography>
+
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
