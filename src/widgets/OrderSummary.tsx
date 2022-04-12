fbba345f23c2ae97fd8b7de2a192751bc4e87a90
Eddiebee <edbeeotudor@gmail.com>
2022-04-12 14:15:19 +0100

    Complete widgets with dummy data inputs

--- src/widgets/OrderSummary.tsx
+++ src/widgets/OrderSummary.tsx
@@ -3,8 +3,13 @@ import StackWithSpaceBetween from "../components/StackWithSpaceBetween";
 
 const OrderSummary = () => {
   return (
-    <Paper elevation={3} sx={{ mt: "2rem", p: ".9rem" }}>
-      <StackWithSpaceBetween title={"subtotal"} cost={12.5} />
+    <Paper
+      elevation={3}
+      sx={{ mt: "2rem", p: ".9rem", bgcolor: "secondary.dark" }}
+    >
+      <StackWithSpaceBetween />
+      <StackWithSpaceBetween />
+      <StackWithSpaceBetween />
     </Paper>
   );
 };
